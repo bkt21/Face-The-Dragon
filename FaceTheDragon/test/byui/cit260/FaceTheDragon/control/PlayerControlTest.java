04b1de84298a41f8daa70b89704f16c9419d0ddb
Jordsterking <mur12016@byui.edu>
2016-02-20 14:34:42 -0700

    Date: 1/28/2016 Check out the test for Player control.  I did the first one that works but it still needs to check the others on your excel table

--- FaceTheDragon/test/byui/cit260/FaceTheDragon/control/PlayerControlTest.java
+++ FaceTheDragon/test/byui/cit260/FaceTheDragon/control/PlayerControlTest.java
@@ -29,11 +29,11 @@ public class PlayerControlTest {
         Character Player = new Character();
         PlayerControl instance = new PlayerControl(); 
         Potion.setQuantity(5);
-        //set the increasse health of the potion....like small potion is 5 and medium 15 and large 30 or somethin
-        //set the max health of the player
-        Player.setHealth(30); // this will have to be less as the max health or the same unless you are checking for errors
+        Potion.setIncreaseHealth(5);
+        Player.setMaxHealth(30);
+        Player.setHealth(20); // this will have to be less as the max health or the same unless you are checking for errors
         int potionsToUse = 1;
-        int expResult = -4;
+        int expResult = 25;
         int result = instance.increaseHealth(Potion, potionsToUse, Player);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
