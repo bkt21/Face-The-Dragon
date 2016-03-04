927c62e64bf38f52572b0e7ff55dfc205bad111f
bkt21 <tho14043@byui.edu>
2016-03-04 16:54:39 -0700

    3/4/16 map view done?

--- FaceTheDragon/src/byui/cit260/FaceTheDragon/view/GameMenuView.java
+++ FaceTheDragon/src/byui/cit260/FaceTheDragon/view/GameMenuView.java
@@ -22,3 +22,4 @@ class GameMenuView extends View {
     }
 
 }
+
--- FaceTheDragon/src/byui/cit260/FaceTheDragon/view/MapView.java
+++ FaceTheDragon/src/byui/cit260/FaceTheDragon/view/MapView.java
@@ -5,11 +5,15 @@
  */
 package byui.cit260.FaceTheDragon.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Brittany
  */
 public class MapView extends View {
+    
+    private String promptMessage;
 
     public MapView() {
         super("\n----------------------------------------------"
@@ -27,15 +31,89 @@ public class MapView extends View {
                 + "\n| Forest | Forest |   AB   | Forest | Dragon |"
                 + "\n|--------|--------|--------|--------|--------|"
                 + "\n Key:"
-                + "\n GW - Good Wizard"
-                + "\n GH - Goblin Hut"
-                + "\n OS - Oger Swamp"
+                + "\n GW - Good Wizard                   N"
+                + "\n GH - Goblin Hut                 W -+- E "
+                + "\n OS - Oger Swamp                    S"
                 + "\n AB - Abandoned Building"
                 + "\n----------------------------------------------");
 }
+    
+     void displayItemMenuView() {
+        boolean done = false;
+        do {
+            String menuOption = this.getMenuOption();
+            if (menuOption.toUpperCase().equals("Q")) {
+                return;
+            }
+
+            done = this.doAction(menuOption);
+        } while (!done);
+    }
+
+    private String getMenuOption() {
+        Scanner keyboard = new Scanner(System.in);
+        String value = "";
+        boolean valid = false;
+
+        while (!valid) {
+            System.out.println("\n" + this.promptMessage);
+            value = keyboard.nextLine();
+            value = value.trim();
+
+            if (value.length() < 1) {
+                System.out.println("\nInvalid value: value cannot be blank");
+                continue;
+            }
+            break;
+        }
+
+        return value;
+    }
+
     @Override
-    public boolean doAction(String value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean doAction(String choice) {
+        this.promptMessage = "\n Which direction do you want to go?";
+        
+        choice = choice.toUpperCase();
+
+        switch (choice) {
+            case "N":
+            case "NORTH":
+                this.moveNorth();
+                break;
+            case "S":
+            case "SOUTH":
+                this.moveSouth();
+                break;
+            case "E":
+            case "EAST":
+                this.moveEast();
+                break;
+            case "W":
+            case "WEST":
+                this.moveWest();
+                break;
+            default:
+                System.out.println("\n*** Invalid Selection**** Try Again");
+                break;
+        }
+        return false;
+    }
+
+    private void moveNorth() {
+        System.out.println("\n***move North was called***");
+    }
+
+    private void moveSouth() {
+       System.out.println("\n***move South was called***");
+    }
+
+    private void moveEast() {
+        System.out.println("\n***move East was called***");
+    }
+
+    private void moveWest() {
+        System.out.println("\n***move West was called***");
     }
     
 }
