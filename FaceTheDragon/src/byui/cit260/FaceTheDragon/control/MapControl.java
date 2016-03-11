f13585dff83fc49d7894cbdc0a1e94a8622cfcc6
Jordsterking <mur12016@byui.edu>
2016-03-10 18:14:11 -0700

    Date: 1/28/2016 Tried to go through team assignment 5.....got to about page 15

---
+++ FaceTheDragon/src/byui/cit260/FaceTheDragon/control/MapControl.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.FaceTheDragon.control;
+
+import byui.cit260.FaceTheDragon.model.Map;
+
+/**
+ *
+ * @author Murray
+ */
+class MapControl {
+
+    static Map createMap() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void moveCharacterToStartingLocation(Map map) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
